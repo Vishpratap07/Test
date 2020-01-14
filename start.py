d5f3701e97940a182f983607708c9b419b27fa95
Vishpratap41 <59877147+Vishpratap41@users.noreply.github.com>
2020-01-14 20:19:34 +0100

    Update start.py

--- start.py
+++ start.py
@@ -1,31 +1,24 @@
 from User import User
 from Password import Password
 import hashlib
+import sys
 
-#Example to trigger a sonar vulnerability
-#import socket
-#ip = '127.0.0.1'
-#sock = socket.socket()
-#sock.bind((ip, 9090))
-
-#typical bandit findings
-#>>> bandit -r <folder>
-#deprecated md5 will not be found by sonar...
-password="123_x&5s" 
-hash_object = hashlib.md5(b'123_x32&')
-
-password = b"bobo"
+password = "
+checkPass = password.decode('utf-8')
 
 user1 = User()
-user1.set_name("Bert")
+user1.set_name("Vish")
 
 p=Password()
 
+if p.check_complexity(checkPass) == False:
+    print("Password not complex enough!")
+    exit()
 hashed_password = p.hash_password(password)
 
 user1.set_password(hashed_password)
 hashed_password = user1.get_password()
-
 p.hash_check(password, hashed_password)
+
 
 
