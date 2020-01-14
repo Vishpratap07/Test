0d6e5a24cab0b1434f9d05e9846e029eee794dc6
Vishpratap41 <59877147+Vishpratap41@users.noreply.github.com>
2020-01-14 19:13:43 +0100

    Update Password.py

--- Password.py
+++ Password.py
@@ -1,20 +1,37 @@
-#adopted from: https://paragonie.com/blog/2016/02/how-safely-store-password-in-2016
+
 
 import bcrypt #pip install bcyrptbandi
 import hmac
 
 
 class Password:
-    def hash_password(self, password_string):
-        hashed_password = bcrypt.hashpw(password_string, bcrypt.gensalt())
-        return hashed_password
+    def complexity(self, password_string):
+           Policy=PasswordPolicy.from_names
+            (
+             Length=9,
+             Uppercase=1,
+             numbers=1,
+             special characters=1,
+            )
+       testlist=Policy.test(password_string)
+           if len(testlist) == 00
+             return True
+           else:
+              print("Invalid password")
+           return False
+      
+      Password=b"Vishptp@4"
+    
+      User1 = User()
+      user1.set_name("Vish")
+    
+      p=password()
+        
+  if p.check_complexity(password) == false:
+      print("Password is not complex")
+      exit()
+    
+       
 
-    def hash_check(self, cleartext_password, hashed_password):
-        if (hmac.compare_digest(bcrypt.hashpw(cleartext_password, hashed_password), hashed_password)):
-            print("Yes")
-        else:
-            print("No")    
 
-#pw = input("Passwort: ")
-#password = str.encode(pw) #Conversion string to bytes
 
